6d31c68e1bc79c5091c6fc567c27c91c36c4a06f
Geoffrey Nix <g.nix@modcloth.com>
2012-08-20 13:41:29 -0700

    fix reference to readme

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "ModCloth, Inc."
 maintainer_email "ops@modcloth.com"
 license          "Apache 2.0"
 description      "Installs/Configures solr"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "java"
